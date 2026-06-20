1524f2b1188ae6dd83d5eb811a9e51790952f50d
Celeste <celestebonillog@gmail.com>
2026-06-20 03:07:26 +0200

    Setup Firebase infrastructure

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.google.services)
 }
 
 android {
@@ -59,4 +60,8 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     implementation(libs.androidx.navigation.compose)
+    implementation(platform(libs.firebase.bom))
+
+    implementation(libs.firebase.auth)
+    implementation(libs.firebase.firestore)
 }
---
+++ app/src/main/java/com/outime/app/presentation/navigation/Routes.kt
@@ -0,0 +1,10 @@
+package com.outime.app.presentation.navigation
+
+object Routes {
+    const val SPLASH = "splash"
+    const val LOGIN = "login"
+    const val REGISTER = "register"
+
+    const val CLIENT_HOME = "client_home"
+    const val BUSINESS_HOME = "business_home"
+}
