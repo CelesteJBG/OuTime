e8d627b63f9c56207f582b8bcb99443ab9e15e54
Celeste <celestebonillog@gmail.com>
2026-08-29 00:17:50 +0200

    Clean project

--- app/src/main/java/com/outime/app/presentation/screens/HomeScreen.kt
+++
@@ -1,9 +0,0 @@
-package com.outime.app.presentation.screens
-
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-
-@Composable
-fun HomeScreen() {
-    Text("Home OuTime")
-}
